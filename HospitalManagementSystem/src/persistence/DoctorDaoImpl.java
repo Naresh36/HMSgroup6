6a36d38dc177a047be44b97cd83daadfdacd127b
anand <anandroushan0707@gmail.com>
2022-04-25 00:07:24 +0530

    dotordaoimpl1 commit

---
+++ HospitalManagementSystem/src/persistence/DoctorDaoImpl.java
@@ -0,0 +1,34 @@
+package persistence;
+
+import java.util.List;
+
+import entity.Doctor;
+import entity.Slot;
+
+public class DoctorDaoImpl implements DoctorDao {
+
+	@Override
+	public List<Doctor> getAllSchedule() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Doctor getScheduleById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Slot> displaySlots() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean selectSlots(int[] arr) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
