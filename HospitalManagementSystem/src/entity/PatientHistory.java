9edcf31e77d2e578c855768fb029c3d697d5fa13
anand <anandroushan0707@gmail.com>
2022-04-25 00:00:20 +0530

    committ

---
+++ HospitalManagementSystem/src/entity/PatientHistory.java
@@ -0,0 +1,20 @@
+package entity;
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class PatientHistory {
+
+	private int patientHistoryId;
+	private String patientHistoryIssue;
+	private LocalDate localDate;
+	private String patientCheckupStatus;
+	
+	
+}
